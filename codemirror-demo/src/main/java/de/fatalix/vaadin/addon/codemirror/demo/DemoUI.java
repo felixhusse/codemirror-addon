c2f8d9cdb89c1dfd4220cbede9e6008cc13e5d21
felixhusse <felix.husse@gmail.com>
2014-03-14 16:40:38 +0100

    - made the DemoUI a little nicer

--- codemirror-demo/src/main/java/de/fatalix/vaadin/addon/codemirror/demo/DemoUI.java
+++ codemirror-demo/src/main/java/de/fatalix/vaadin/addon/codemirror/demo/DemoUI.java
@@ -1,4 +1,4 @@
-package de.fatalix.vaadin.addon.timlinejs.demo;
+package de.fatalix.vaadin.addon.codemirror.demo;
 
 
 import javax.servlet.annotation.WebServlet;
@@ -9,12 +9,10 @@ import com.vaadin.annotations.Title;
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
-import com.vaadin.shared.ui.label.ContentMode;
+import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Button;
-import com.vaadin.ui.Label;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
-import com.vaadin.ui.Window;
 import de.fatalix.vaadin.addon.codemirror.CodeMirror;
 
 
@@ -35,7 +33,12 @@ public class DemoUI extends UI {
                                                 "  }\n" +
                                                 "  return find(1, \"1\");\n" +
                                                 "}";
-
+    
+    private static final String exampleCode2 =   "function findDaSequenceeee(goal) {\n" +
+                                                "  function doSomeSearching(start, history) {\n" +
+                                                "  }\n" +
+                                                "  return find(1, \"1\");\n" +
+                                                "}";
     @WebServlet(value = "/*", asyncSupported = true)
     @VaadinServletConfiguration(productionMode = false, ui = DemoUI.class)
     public static class Servlet extends VaadinServlet {
@@ -50,24 +53,28 @@ public class DemoUI extends UI {
             layout.setStyleName("demoContentLayout");
             
             final CodeMirror codeMirror = new CodeMirror();
-            codeMirror.setSizeFull();
+            codeMirror.setHeight(500, Unit.PIXELS);
+            codeMirror.setWidth(600, Unit.PIXELS);
             codeMirror.setCode(exampleCode);
             
-            Button button = new Button("", new Button.ClickListener() {
+            Button button = new Button("Load some code", new Button.ClickListener() {
 
                 @Override
                 public void buttonClick(Button.ClickEvent event) {
-                    Window dialog = new Window("Code:");
-                    dialog.setModal(true);
-                    dialog.setContent(new VerticalLayout(new Label(codeMirror.getCode(), ContentMode.PREFORMATTED)));
-                    dialog.setHeight(400,Unit.PIXELS);
-                    dialog.setWidth(500,Unit.PIXELS);
-                    UI.getCurrent().addWindow(dialog);
+                    codeMirror.setCode(exampleCode2);
+                    
+//                    Window dialog = new Window("Code:");
+//                    dialog.setModal(true);
+//                    dialog.setContent(new VerticalLayout(new Label(codeMirror.getCode(), ContentMode.PREFORMATTED)));
+//                    dialog.setHeight(400,Unit.PIXELS);
+//                    dialog.setWidth(500,Unit.PIXELS);
+//                    UI.getCurrent().addWindow(dialog);
                 }
             });
             
             layout.addComponents(codeMirror,button);
             layout.setExpandRatio(codeMirror, 1.0f);
+            layout.setComponentAlignment(codeMirror, Alignment.MIDDLE_CENTER);
             layout.setSizeFull();
             
             
